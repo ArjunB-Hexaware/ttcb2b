dea7ef0db2807895eeed977c0346e409e3b7b6f6
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-04-19 15:49:21 +0530

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -7,7 +7,7 @@ This file is part of the Innovation LAB - Offline Bot.
 
 
 define({    
-    eventAllow :["Yes, please", "Overview","Your Itinerary","What's included","Mail Package Info","Trafalgar Experience","Guest Photos","No, thanks", "Italy","Europe","Tour Packages","Wonders of Italy *NEW* Summer 2018","May","June","July","August","September","October","November","December","8 - 14 days (50)","14 + days (27)","1 - 7 days (3)","US$0 - $1000(1)","US$1001 - $1500(3)","US$1501 - $2000(13)","US$2001 - $2500(12)","US$2501 - $3000(10)","US$3001 - $5000(27)","US$5001 - $10000(12)","1","2","3","4","5","6","7","8","9","10","Yes","No"],
+    eventAllow :["Yes, please", "Overview","Your Itinerary","What's included","Mail Package Info","Trafalgar Experience","Guest Photos","No, thanks", "Italy","Europe","Tour Packages","Wonders of Italy *NEW* Summer 2018","May","June","July","August","September","October","November","December","8 - 14 days (50)","14 + days (27)","1 - 7 days (3)","US$0 - $1000(1)","US$1001 - $1500(3)","US$1501 - $2000(13)","US$2001 - $2500(12)","US$2501 - $3000(10)","US$3001 - $5000(27)","US$5001 - $10000(12)","1","2","3","4","5","6","7","8","9","10","Yes","No","Trip options"],
     chatServerURL : "https://ttcb2b.herokuapp.com/dialogflowAPI",
   //chatServerURL : "http://localhost:3002/dialogflowAPI",
 	accessToken:"123445",
