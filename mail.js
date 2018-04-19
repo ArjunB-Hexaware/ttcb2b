4fa700065cfd6955a91258127307a496740f011d
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-04-19 21:03:39 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -57,7 +57,7 @@ mailPackageDetails:function(reqBody){
 			// html:  "<div>Hi,</div><div style='margin-left:10px;margin-bottom:5px'>Greetings from TTC!!!</div><center>Package info- <i>WONDERS OF ITALY *NEW* - 11 DAYS, 9 CITIES</i></center><center><img src='./public/images/italy-wonders.png' alt='Wonders-Italy' height='160' width='160' align='middle'></center><div style='margin-top:15px;'><b>ABOUT THIS TRIP</b></div><div>Pay tribute to the extraordinary masterpieces of Italy's greats - the captivating arias of Puccini and splendour of da Vinci, Michelangelo and Botticelli's talents in their full glory throughout this enduring exploration of Italy's alluring art, architecture and old-world charm. See the city of one hundred churches, the imposing towers of picturesque San Gimignano and the seas of pastel pinks, yellows and blues that cling dramatically to the Cinque Terre's Ligurian cliffs.</div><ul><li>10 NIGHTS - accommodation</li><li>15 MEALS - 10 hot buffet breakfasts,1 lunch (Be My Guest),4 dinners with local wine</li><li>ON-TRIP TRANSPORT - All transport shown.</li></ul><div>Regards,</div><div>TTC Team</div>",
 			attachments: [{
         filename: 'italy-wonders.png',
-        path: './public/images/italy-wonders',
+        path: './public/images/italy-wonders.png',
 	//content:fs.createReadStream('./public/images/italy-wonders.png'),
         cid: 'italy@img' //same cid value as in the html img src
     }]
